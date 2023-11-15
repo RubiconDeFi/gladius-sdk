2e0326957500e9a0b078f4353fdbcd7f24542e91
Ethereal Iska <etherealiska@gmail.com>
2023-11-15 15:36:01 -0500

    Add deadline padding to ensure ODOS has extra time

--- src/utils/BookBuilder.ts
+++ src/utils/BookBuilder.ts
@@ -25,6 +25,16 @@ interface SimpleBook {
     bids: GenericOrderWithData[];
 }
 
+interface Config { 
+    chainID: number;
+    assetAddress: string;
+    quoteAddress: string;
+    gladiusUrl: string;
+    rpcUrl: string;
+    gladiusPollTime?: number;
+    expirationPadding?: number;
+}
+
 class BookBuilder extends EventEmitter {
     private chainID: number;
     private assetAddress: string;
@@ -36,8 +46,9 @@ class BookBuilder extends EventEmitter {
     private assetDecimals: number | undefined;
     private quoteDecimals: number | undefined;
     private rpcProvider: JsonRpcProvider;
+    private expirationPadding: number;
 
-    constructor(config: { chainID: number; assetAddress: string; quoteAddress: string; gladiusUrl: string; rpcUrl: string; gladiusPollTime?: number }) {
+    constructor(config: Config) {
         super();
         this.chainID = config.chainID;
         this.assetAddress = config.assetAddress;
@@ -45,13 +56,11 @@ class BookBuilder extends EventEmitter {
         this.gladiusUrl = config.gladiusUrl;
         this.rpcUrl = config.rpcUrl;
         this.gladiusPollTime = config.gladiusPollTime || 500;
+        this.expirationPadding = config.expirationPadding || 10;
         this.book = { asks: [], bids: [] };
 
         this.rpcProvider = new JsonRpcProvider(this.rpcUrl, this.chainID);
         this.initializeTokenDecimals().then(() => {
-            // console.log("this is the asset decimals", this.assetDecimals);
-            // console.log("this is the quote decimals", this.quoteDecimals);
-
             this.listenToGladiusBook();
         });
     }
@@ -136,45 +145,12 @@ class BookBuilder extends EventEmitter {
                 }
                 try {
                     const parsedOrderData = parseOrderWithChainId(ask.encodedOrder, this.chainID)
-                    // console.log("this is the parsed order ASK data", parsedOrderData);
-
-                    // e.g.
-                    //     info: {
-                    //       reactor: '0xcB23e6c82c900E68d6F761bd5a193a5151A1D6d2',
-                    //       swapper: '0x00b20eEd81122763A393F11765D821eA0B8D4d5a',
-                    //       nonce: BigNumber { _hex: '0xa8', _isBigNumber: true },
-                    //       deadline: 1698670880,
-                    //       additionalValidationContract: '0x0000000000000000000000000000000000000000',
-                    //       additionalValidationData: '0x',
-                    //       decayStartTime: 1698670870,
-                    //       decayEndTime: 1698670879,
-                    //       exclusiveFiller: '0x0000000000000000000000000000000000000000',
-                    //       exclusivityOverrideBps: BigNumber { _hex: '0x00', _isBigNumber: true },
-                    //       input: {
-                    //         token: '0x4200000000000000000000000000000000000006',
-                    //         startAmount: [BigNumber],
-                    //         endAmount: [BigNumber]
-                    //       },
-                    //       outputs: [ [Object] ]
-                    //     },
-                    //     chainId: 10,
-                    //     _permit2Address: undefined,
-                    //     permit2Address: '0x000000000022d473030f116ddee9f6b43ac78ba3'
-                    //   }
 
                     // deadline filtering
                     // TODO: also introduce deadline leadup time for added cushion!
-                    if (parsedOrderData.info.deadline < Math.floor(Date.now() / 1000)) {
-
-                        // console.log("deadline", parsedOrderData.info.deadline);
-                        // console.log("now", Math.floor(Date.now() / 1000));
-                        
-                        // console.log("this order is expired");
+                    if (parsedOrderData.info.deadline < Math.floor(Date.now() / 1000) - this.expirationPadding) {
                         continue;
                     }
-
-                    // console.log("this order is not expired");
-                    
 
                     // Perform any more filtering as needed
                     if (quoteOutputEndAmount == quoteOutputStartAmount && assetInputEndAmount == assetInputStartAmount) {
@@ -202,30 +178,23 @@ class BookBuilder extends EventEmitter {
             }
 
             if (bid) {
-                // console.log("got a bid we need to parse", bid);
                 const quoteInputStartAmount = bid.input.startAmount;
                 const quoteInputEndAmount = bid.input.endAmount;
                 const assetOutputStartAmount = bid.outputs[0].startAmount;
                 const assetOutputEndAmount = bid.outputs[0].endAmount;
 
                 if (bid.input.token != this.quoteAddress || bid.outputs[0].token != this.assetAddress) {
-                    // console.log("bid inputs token does not match quote address");
                     continue;
                 }
 
                 try {
                     const parsedOrderData = parseOrderWithChainId(bid.encodedOrder, this.chainID)
-                    // console.log("this is the parsed order BID data", parsedOrderData);
 
                     // deadline filtering
                     // TODO: also introduce deadline leadup time for added cushion!
-                    if (parsedOrderData.info.deadline < Math.floor(Date.now() / 1000)) {
-                        // console.log("this order is expired");
+                    if (parsedOrderData.info.deadline < Math.floor(Date.now() / 1000) - this.expirationPadding) {
                         continue;
                     }
-                    // console.log("this order is not expired");
-
-                    // Perform any more filtering as needed
 
                     if (quoteInputEndAmount == quoteInputStartAmount && assetOutputEndAmount == assetOutputStartAmount) {
                         // Non decaying order we can parse
@@ -246,11 +215,8 @@ class BookBuilder extends EventEmitter {
 
                 } catch (error) {
                     console.log("error parsing BID order", error);
-
                 }
-
             }
-
         }
 
         // Implement the parsing logic as per your requirements
@@ -265,21 +231,6 @@ class BookBuilder extends EventEmitter {
         return this.book;
     }
 
-    // // Method to subscribe and listen for updates
-    // public subscribeToUpdate() {
-    //     // Implement logic to subscribe and emit updates
-    //     // this.emit('update', this.book);
-    // }
-
-    // // Method for market buy
-    // public marketBuy(quantity: number, isAsset: boolean): void {
-    //     // Implement market buy logic
-    // }
-
-    // // Method for market sell
-    // public marketSell(quantity: number, isAsset: boolean): void {
-    //     // Implement market sell logic
-    // }
 }
 
 export default BookBuilder;
